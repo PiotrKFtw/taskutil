d04a118886b6caefac5342d3a9c73f0b2b6c4ada
NotJansel <janselfritz096@gmail.com>
2021-03-18 18:27:19 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskutil
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            gfn gfn = new gfn();
+
+            if (args.Length == 0)
+            {
+                gfn.NoArgs();
+            }
+            else
+            {
+                if (args[0] == "/?")
+                {
+                    gfn.Help();
+                }
+                if (args[0] == "/k")
+                {
+                    if (args.Length == 1)
+                    {
+                        gfn.NoProcessTypeFinder();
+                    }
+                    if (args[1] == "/p")
+                    {
+                        gfn.ProcessKillPID(args[2]);
+                    }
+                    if (args[1] == "/n")
+                    {
+                        gfn.ProcessKill(args[2]);
+                    }
+                }
+                if (args[0] == "/s")
+                {
+                    if (args.Length == 1)
+                    {
+                        gfn.NoProcessArg();
+                    }
+                    if (args[1] == "/p")
+                    {
+
+                    }
+                    if (args[1] == "/n")
+                    {
+
+                    }
+                }
+                if (args[0] == "/r")
+                {
+                    if (args.Length == 1)
+                    {
+                        gfn.NoProcessArg();
+                    }
+                    if (args[1] == "/p")
+                    {
+
+                    }
+                    if (args[1] == "/n")
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ gfn.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+[Flags]
+public enum ThreadAccess : int
+{
+    TERMINATE = (0x0001),
+    SUSPEND_RESUME = (0x0002),
+    GET_CONTEXT = (0x0008),
+    SET_CONTEXT = (0x0010),
+    SET_INFORMATION = (0x0020),
+    QUERY_INFORMATION = (0x0040),
+    SET_THREAD_TOKEN = (0x0080),
+    IMPERSONATE = (0x0100),
+    DIRECT_IMPERSONATION = (0x0200)
+}
+
+public static class ProcessExtension
+{
+    [DllImport("kernel32.dll")]
+    static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+    [DllImport("kernel32.dll")]
+    static extern uint SuspendThread(IntPtr hThread);
+    [DllImport("kernel32.dll")]
+    static extern int ResumeThread(IntPtr hThread);
+
+    public static void Suspend(this Process process)
+    {
+        foreach (ProcessThread thread in process.Threads)
+        {
+            var pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)thread.Id);
+            if (pOpenThread == IntPtr.Zero)
+            {
+                break;
+            }
+            SuspendThread(pOpenThread);
+        }
+    }
+    public static void Resume(this Process process)
+    {
+        foreach (ProcessThread thread in process.Threads)
+        {
+            var pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)thread.Id);
+            if (pOpenThread == IntPtr.Zero)
+            {
+                break;
+            }
+            ResumeThread(pOpenThread);
+        }
+    }
+}
+
+namespace taskutil
+{
+    class gfn
+    {
+
+        public static void NoArgs()
+        {
+            Console.WriteLine("There were no arguments. Do taskutil /? for help.");
+            return;
+        }
+
+        public static void Help()
+        {
+            Console.WriteLine("Welcome to Taskutil. This is a replacement taskkill for Windows, if the original taskkill is");
+            Console.WriteLine("blocked somehow.");
+            Console.WriteLine("Usage:" + 
+                "\n Terminate Process:" + 
+                "\n  taskutil /k /p <PID>" + 
+                "\n  taskutil /k /n <Processname>" + 
+                "\n " + 
+                "\n Suspend Process:" + 
+                "\n  taskutil /s /p <PID>" + 
+                "\n  taskutil /s /n <ProcessName>" + 
+                "\n " + 
+                "\n Resume Process:" +
+                "\n  taskutil /r /p <PID>" +
+                "\n  taskutil /r /n <Processname>");
+            return;
+        }
+
+        public static void NoProcessTypeFinder()
+        {
+            Console.WriteLine("You didn\'t specify a Process Type to find.\nUse /p for finding it by the PID or /n for finding it by Name.");
+            return;
+        }
+
+        public static void NoProcessArg()
+        {
+            Console.WriteLine("Did u specify a Process?");
+            return;
+        }
+
+        public static void ProcessKill(string process)
+        {
+            foreach (var pwet in Process.GetProcessesByName(process))
+            {
+                try
+                {
+                    pwet.Kill();
+                    Console.WriteLine(process + " was terminated.");
+                }
+                catch
+                {
+                    Console.WriteLine("The process couldn\'t be terminated. Does it even exist or is it protected?");
+                    return;
+                }
+            }
+        }
+
+        public static void ProcessSuspend(string process)
+        {
+
+            foreach (var pwet in Process.GetProcessesByName(process))
+            {
+                try
+                {
+                    pwet.Suspend();
+                    Console.WriteLine(process + " was suspended.");
+                }
+                catch
+                {
+                    Console.WriteLine("The process couldn\'t be suspended. Does it even exist or is it protected?");
+                    return;
+                }
+
+            }
+        }
+
+        public static void ProcessResume(string process)
+        {
+            foreach (var pwet in Process.GetProcessesByName(process))
+            {
+                try
+                {
+                    pwet.Resume();
+                    Console.WriteLine(process + " was resumed.");
+                }
+                catch
+                {
+                    Console.WriteLine("The process couldn\'t be resumed. Does it even exist or is it protected?");
+                    return;
+                }
+            }
+        }
+
+        public static void ProcessKillPID(string process)
+        {
+            int id = Convert.ToInt32(process);
+            Process pwet = Process.GetProcessById(id);
+            string processname = pwet.ProcessName;
+            try
+            {
+                pwet.Kill();
+                Console.WriteLine(processname + " was terminated.");
+            }
+            catch
+            {
+                Console.WriteLine("The process couldn\'t be terminated. Does it even exist or is it protected?");
+                return;
+            }
+
+        }
+
+        public static void ProcessSuspendPID(string process)
+        {
+            int id = Convert.ToInt32(process);
+            Process pwet = Process.GetProcessById(id);
+            string processname = pwet.ProcessName;
+            try
+            {
+                pwet.Suspend();
+                Console.WriteLine(processname + " was suspended.");
+            }
+            catch
+            {
+                Console.WriteLine("The process couldn\'t be suspended. Does it even exist or is it protected?");
+                return;
+            }
+        }
+
+        public static void ProcessResumePID(string process)
+        {
+            int id = Convert.ToInt32(process);
+            Process pwet = Process.GetProcessById(id);
+            string processname = pwet.ProcessName;
+            try
+            {
+                pwet.Resume();
+                Console.WriteLine(processname + " was resumed.");
+            }
+            catch
+            {
+                Console.WriteLine("The process couldn\'t be resumed. Does it even exist or is it protected?");
+                return;
+            }
+        }
+    }
+}
